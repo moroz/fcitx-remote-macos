a5e448a3e19cc1cdf4d8d2258771298886bd5cbc
Karol Moroz <karol@moroz.dev>
2023-12-15 04:03:24 +0700

    Get it working

--- Switcher.swift
+++ Switcher.swift
@@ -1,37 +1,72 @@
 import Carbon
-import Cocoa
-import InputMethodKit
 
-var en: TISInputSource?
-var zh: TISInputSource?
+let ZH_ID = "com.sogou.inputmethod.sogouWB.wubi"
+let EN_ID = "org.unknown.keylayout.CustomDvorak"
 
 func getInputSourceId(_ input: TISInputSource) -> String? {
-    guard let id = TISGetInputSourceProperty(input, kTISPropertyInputSourceID) else { return nil }
-    return Unmanaged<AnyObject>.fromOpaque(id).takeUnretainedValue() as? String
+  guard let id = TISGetInputSourceProperty(input, kTISPropertyInputSourceID) else { return nil }
+  return Unmanaged<AnyObject>.fromOpaque(id).takeUnretainedValue() as? String
 }
 
-let inputSourceNSArray = TISCreateInputSourceList(nil, false).takeRetainedValue() as NSArray
-let inputSourceList = inputSourceNSArray as! [TISInputSource]
+func isEnabled(id: String) -> Bool {
+  let keyboard = TISCopyCurrentKeyboardInputSource().takeRetainedValue()
+  return getInputSourceId(keyboard) == id
+}
 
-for input in inputSourceList {
-    guard let id = TISGetInputSourceProperty(input, kTISPropertyInputSourceID) else { break }
-    let unwrapped = Unmanaged<AnyObject>.fromOpaque(id).takeUnretainedValue() as! String
-    switch unwrapped {
-    case "org.unknown.keylayout.CustomDvorak":
-        en = input
-    case "com.sogou.inputmethod.sogouWB.wubi":
-        zh = input
-    default:
-        continue
+func isNonLatinEnabled() -> Bool {
+  return isEnabled(id: ZH_ID)
+}
+
+func isLatinEnabled() -> Bool {
+  return isEnabled(id: EN_ID)
+}
+
+func enableLatin() {
+  guard !isLatinEnabled() else { return }
+  guard let source = findInputSource(id: EN_ID) else { return }
+  TISSelectInputSource(source)
+}
+
+func enableNonLatin() {
+  guard !isNonLatinEnabled() else { return }
+  guard let source = findInputSource(id: ZH_ID) else { return }
+  TISSelectInputSource(source)
+}
+
+func findInputSource(id needleId: String) -> TISInputSource? {
+  let inputSourceNSArray = TISCreateInputSourceList(nil, false).takeRetainedValue() as NSArray
+  let inputSourceList = inputSourceNSArray as! [TISInputSource]
+
+  for input in inputSourceList {
+    if let id = getInputSourceId(input), id == needleId {
+      return input
     }
+  }
+  return nil
 }
 
-guard let zh = zh, let en = en else { exit(1) }
+func toggleInput() {
+  if isNonLatinEnabled() {
+    enableLatin()
+  } else {
+    enableNonLatin()
+  }
+}
 
-let keyboard = TISCopyCurrentKeyboardInputSource().takeRetainedValue()
+if CommandLine.argc == 1 {
+  if isNonLatinEnabled() {
+    print("2")
+  } else {
+    print("1")
+  }
+  exit(0)
+}
 
-if keyboard == zh {
-    TISSelectInputSource(en)
-} else {
-    TISSelectInputSource(zh)
+switch CommandLine.arguments[1] {
+case "-c":
+  enableLatin()
+case "-o", "-e":
+  enableNonLatin()
+default:
+  break
 }
